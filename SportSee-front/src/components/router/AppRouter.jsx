63d78a04ab0431ebc331d28d58dca7776c563e37
Krehion <krehion@hotmail.com>
2025-01-08 11:17:05 +0100

    :poop: redirect missing/empty pages to dashboard (temp solution awaiting 404)

--- SportSee-front/src/components/router/AppRouter.jsx
+++ SportSee-front/src/components/router/AppRouter.jsx
@@ -8,6 +8,8 @@ const AppRouter = () => {
 				<Route path="/" element={<Navigate to="/user/12" />} />{" "}
 				{/* redirect to user 12 by default for kickstarting the project, remove once there's a homepage */}
 				<Route path="/user/:id" element={<Dashboard />} />
+				<Route path="*" element={<Dashboard />} />{" "}
+				{/* redirect missing pages to dashboard by default for kickstarting the project, remove once there's a 404 */}
 			</Routes>
 		</BrowserRouter>
 	);
