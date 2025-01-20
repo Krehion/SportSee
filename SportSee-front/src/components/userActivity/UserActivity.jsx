ea1f2c4c4ef78ba3a1a3cda82d16bb6345f2a832
Krehion <krehion@hotmail.com>
2025-01-20 18:39:14 +0100

    :lipstick: style user activity chart's legend

--- SportSee-front/src/components/userActivity/UserActivity.jsx
+++ SportSee-front/src/components/userActivity/UserActivity.jsx
@@ -23,18 +23,58 @@ const UserActivity = ({ userId }) => {
 	// Extract the sessions array
 	const data = userActivity.sessions;
 
+	const CustomLegend = ({ payload }) => {
+		return (
+			<div
+				style={{
+					position: "absolute",
+					height: "auto",
+					right: "10px",
+					top: "4px",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center"
+				}}>
+				{payload.map((entry, index) => (
+					<div
+						key={`item-${index}`}
+						style={{
+							display: "flex",
+							alignItems: "center",
+							marginLeft: "30px"
+						}}>
+						<div
+							style={{
+								width: "8px",
+								height: "8px",
+								backgroundColor: entry.color,
+								marginRight: "10px",
+								borderRadius: "50%"
+							}}></div>
+						<span className="activity-chart--legend">{entry.value}</span>
+					</div>
+				))}
+			</div>
+		);
+	};
+
 	return (
 		<div className="activity-chart">
 			<h2 className="chart-title">Activité quotidienne</h2>
 			<ResponsiveContainer width="100%" height={300}>
 				<BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-					<CartesianGrid strokeDasharray="3 3" horizontal={true} vertical={false} />
+					<CartesianGrid stroke="#DEDEDE" strokeDasharray="3 3" horizontal={true} vertical={false} />
 					<XAxis dataKey="day" />
 					<YAxis />
 					<Tooltip />
-					<Bar dataKey="kilogram" fill={colorGreyDark} name="Poids (kg)" shape={<CustomBar />} />
-					<Bar dataKey="calories" fill={colorRedDark} name="Calories brûlées (kCal)" shape={<CustomBar />} />
-					<Legend verticalAlign="top" height={36} />
+					<Bar dataKey="kilogram" fill={colorGreyDark || "#282d30"} name="Poids (kg)" shape={<CustomBar />} />
+					<Bar
+						dataKey="calories"
+						fill={colorRedDark || "#e60000"}
+						name="Calories brûlées (kCal)"
+						shape={<CustomBar />}
+					/>
+					<Legend content={(props) => <CustomLegend {...props} />} verticalAlign="top" />
 				</BarChart>
 			</ResponsiveContainer>
 		</div>
