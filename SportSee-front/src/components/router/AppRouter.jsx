493c8f56c9c224cfa2de3900b6ad2bd8aa1b8273
Krehion <krehion@hotmail.com>
2025-02-05 17:13:54 +0100

    :wrench: change BrowserRouter to HashRouter to fix gh pages deployment

--- SportSee-front/src/components/router/AppRouter.jsx
+++ SportSee-front/src/components/router/AppRouter.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "../../pages/dashboard/Dashboard.jsx";
 
 const AppRouter = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<Navigate to="/user/12" />} />{" "}
 				{/* redirect to user 12 by default for kickstarting the project, remove once there's a homepage */}
 				<Route path="/user/:id" element={<Dashboard />} />
-				<Route path="*" element={<Dashboard />} />{" "}
+				<Route path="*" element={<Navigate to="/user/12" />} />{" "}
 				{/* redirect missing pages to dashboard by default for kickstarting the project, remove once there's a 404 */}
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
